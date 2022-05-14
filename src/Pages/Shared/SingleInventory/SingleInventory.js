ed89e9be3434222a0afc4adaaafa40a78dc35aed
akash7766 <mraakash01932@gmail.com>
2022-05-14 18:59:03 +0600

    add quantity and deliver option implement

--- src/Pages/Shared/SingleInventory/SingleInventory.js
+++ src/Pages/Shared/SingleInventory/SingleInventory.js
@@ -3,13 +3,48 @@ import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const SingleInventory = () => {
+  // load specific items by id
   const { id } = useParams();
   const [product, setProduct] = useState({});
   useEffect(() => {
     fetch(`http://localhost:5000/product/${id}`)
       .then((res) => res.json())
       .then((data) => setProduct(data));
-  }, []);
+  }, [product]);
+
+  // deliver function
+  const deliver = () => {
+    const previousQuantity = parseInt(product.quantity);
+    const newQuantity = previousQuantity - 1;
+    fetch(`http://localhost:5000/update/${id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "PUT",
+      body: JSON.stringify({ quantity: newQuantity }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  // addQuntity function
+  const addQuntity = (e) => {
+    e.preventDefault();
+    const previousQuantity = parseInt(product.quantity);
+    const quantityInput = parseInt(e.target.quantity.value);
+    const newQuantity = previousQuantity + quantityInput;
+    fetch(`http://localhost:5000/update/${id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "PUT",
+      body: JSON.stringify({ quantity: newQuantity }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div className="container p-5">
@@ -18,7 +53,7 @@ const SingleInventory = () => {
       </h2>
       <div className="row">
         <div className=" col-md-8 col-lg-6 mx-auto">
-          <Form>
+          <Form onSubmit={addQuntity}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Control
                 type="text"
@@ -47,7 +82,12 @@ const SingleInventory = () => {
             </Form.Group>
             <label htmlFor="">Add more abailable Quantity</label>
             <Form.Group className="mb-3" controlId="formBasicPrice">
-              <Form.Control type="number" placeholder="Add Quantity" />
+              <Form.Control
+                name="quantity"
+                type="number"
+                placeholder="Add Quantity"
+                required
+              />
             </Form.Group>
             <Button variant="outline-primary" type="submit" className=" mb-4">
               Add Quantity
@@ -55,7 +95,7 @@ const SingleInventory = () => {
           </Form>
           <div>
             <h5>If already delivered any product then send response.</h5>
-            <button className="btn btn-outline-primary">
+            <button onClick={deliver} className="btn btn-outline-primary">
               Delivered One Item
             </button>
           </div>
